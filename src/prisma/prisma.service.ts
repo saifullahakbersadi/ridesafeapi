e4e8c07b29e59af3e991183f90fe81b725c56ca0
Saif <saifullahakber.sad@star.com.au>
2026-07-22 20:28:05 +1000

    Add prisma

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,23 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../../generated/prisma/client';
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
+@Injectable()
+export class PrismaService
+extends PrismaClient
+implements OnModuleInit {
+
+constructor() {
+  super({ adapter });
+}
+
+async onModuleInit() {
+  await this.$connect();
+}
+
+}
